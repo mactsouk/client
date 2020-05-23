10b72234e860119fb301c58b906ff905cc8ff5a6
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2020-05-23 07:55:46 +0300

    Minor changes

--- cmd/root.go
+++ cmd/root.go
@@ -22,13 +22,8 @@ var UserData handlers.UserPass
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "client",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A client for a RESTful server",
+	Long:  `This utility is a client for a RESTful server`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
