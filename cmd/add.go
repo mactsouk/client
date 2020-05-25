4caf99bf4f2b5c44be12a6391321e5c855150514
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2020-05-25 09:45:42 +0300

    Minor corrections

--- cmd/add.go
+++ cmd/add.go
@@ -1,7 +1,7 @@
 /*
 Copyright © 2020 Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
-
 */
+
 package cmd
 
 import (
@@ -15,21 +15,13 @@ var addCmd = &cobra.Command{
 	Use:   "add",
 	Short: "A brief description of your command",
 	Long:  ``,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("add called")
-	},
+	Run:   Add,
+}
+
+func Add(cmd *cobra.Command, args []string) {
+	fmt.Println("add called")
 }
 
 func init() {
 	rootCmd.AddCommand(addCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// addCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
