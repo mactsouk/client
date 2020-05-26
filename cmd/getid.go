26b468b33af76bf9800e036bddb749b0323eb3fc
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2020-05-26 09:53:54 +0300

    Implement getid command

--- cmd/getid.go
+++ cmd/getid.go
@@ -5,8 +5,12 @@ Copyright © 2020 Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
 package cmd
 
 import (
+	"bytes"
 	"fmt"
+	"net/http"
+	"time"
 
+	"github.com/mactsouk/handlers"
 	"github.com/spf13/cobra"
 )
 
@@ -20,6 +24,40 @@ var getidCmd = &cobra.Command{
 
 func GetID(cmd *cobra.Command, args []string) {
 	fmt.Println("getid called")
+	userpass := handlers.UserPass{Username: USERNAME, Password: PASSWORD}
+
+	// bytes.Buffer is both a Reader and a Writer
+	buf := new(bytes.Buffer)
+	err := userpass.ToJSON(buf)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	req, err := http.NewRequest("GET", SERVER+PORT+"/getid", buf)
+	if err != nil {
+		fmt.Println("GetID – Error in req: ", err)
+		return
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	c := &http.Client{
+		Timeout: 15 * time.Second,
+	}
+
+	resp, err := c.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Full response:", resp)
+		fmt.Println("Response code:", resp.StatusCode)
+		return
+	}
+	fmt.Println(resp.Body)
+	defer resp.Body.Close()
 }
 
 func init() {
